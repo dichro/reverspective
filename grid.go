034c749901b07696e5a74dead81e053618310b2a
Mikolaj Habryn <dichro@rcpt.to>
2014-10-19 18:50:40 -0700

    gofmt

--- grid.go
+++ grid.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"os"
-	"math"
+	"bitbucket.org/zombiezen/gopdf/pdf"
 	"fmt"
-        "bitbucket.org/zombiezen/gopdf/pdf"
+	"math"
+	"os"
 )
 
 func grid(canvas *pdf.Canvas, side, stretch pdf.Unit) {
 	rows := pdf.Unit(6)
 	delta := side * (stretch - 1) / 2
 	for i := pdf.Unit(0); i < rows; i++ {
-		x1 := i * side / rows;
-		x2 := (i+1) * side / rows;
-		y1 := side / 2 + delta * i / rows
-		y2 := side / 2 + delta * (i + 1) / rows
+		x1 := i * side / rows
+		x2 := (i + 1) * side / rows
+		y1 := side/2 + delta*i/rows
+		y2 := side/2 + delta*(i+1)/rows
 		path := pdf.Path{}
 		path.Move(pdf.Point{x1, y1})
 		path.Line(pdf.Point{x2, y2})
@@ -24,7 +24,7 @@ func grid(canvas *pdf.Canvas, side, stretch pdf.Unit) {
 		canvas.Stroke(&path)
 	}
 	path := pdf.Path{}
-	for i := pdf.Unit(0); i < rows / 2; i++ {
+	for i := pdf.Unit(0); i < rows/2; i++ {
 		y1 := (i / rows) * side
 		y2 := y1 * stretch
 		path.Move(pdf.Point{0, y1})
@@ -44,31 +44,31 @@ func main() {
 	doc := pdf.New()
 	canvas := doc.NewPage(width, height)
 	canvas.Push()
-	canvas.Translate((width - square) / 2, height / 2)
+	canvas.Translate((width-square)/2, height/2)
 	grid(canvas, square, 1)
 	canvas.Pop()
 	canvas.Push()
-	canvas.Translate(width / 2, (height + square) / 2)
+	canvas.Translate(width/2, (height+square)/2)
 	canvas.Rotate(math.Pi / 2)
-	canvas.Scale(float32((height - square) / 2 / square), 1)
-	grid(canvas, square, width / square)
+	canvas.Scale(float32((height-square)/2/square), 1)
+	grid(canvas, square, width/square)
 	canvas.Pop()
 	canvas.Push()
-	canvas.Translate(width / 2, (height - square) / 2)
+	canvas.Translate(width/2, (height-square)/2)
 	canvas.Rotate(-math.Pi / 2)
-	canvas.Scale(float32((height - square) / 2 / square), 1)
-	grid(canvas, square, width / square)
+	canvas.Scale(float32((height-square)/2/square), 1)
+	grid(canvas, square, width/square)
 	canvas.Pop()
 	canvas.Push()
-	canvas.Translate((width - square) / 2, height / 2)
+	canvas.Translate((width-square)/2, height/2)
 	canvas.Rotate(math.Pi)
-	canvas.Scale(float32((width - square) / 2 / square), 1)
-	grid(canvas, square, height / square)
+	canvas.Scale(float32((width-square)/2/square), 1)
+	grid(canvas, square, height/square)
 	canvas.Pop()
 	canvas.Push()
-	canvas.Translate((width + square) / 2, height / 2)
-	canvas.Scale(float32((width - square) / 2 / square), 1)
-	grid(canvas, square, height / square)
+	canvas.Translate((width+square)/2, height/2)
+	canvas.Scale(float32((width-square)/2/square), 1)
+	grid(canvas, square, height/square)
 	canvas.Pop()
 	canvas.Close()
 	err := doc.Encode(os.Stdout)
